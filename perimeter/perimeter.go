76923c4332e6a78123ceb13753eeb5d082537112
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-04-25 17:04:48 +0800

    update perimeter

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,10 +1,24 @@
 package perimeter
 
+import "math"
+
 // 重构
 
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
 
 func Perimeter(rectangle Rectangle) float64 {
