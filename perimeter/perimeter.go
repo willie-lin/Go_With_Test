daecc74c801c115f982bb4fe8f9df3f227fc5247
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-04-25 10:55:27 +0800

    update shapes

---
+++ perimeter/perimeter.go
@@ -0,0 +1,16 @@
+package perimeter
+
+// 重构
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
