7397f8cb410ee9ec44418a71d390e61e5a0e976d
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-04-30 18:36:41 +0800

    update interface

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -15,6 +15,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
 		got := rectangle.Area()
