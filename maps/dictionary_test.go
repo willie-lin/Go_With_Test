27606ce3967fe64252ccd2779434361374bd8ac7
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-01 19:17:41 +0800

    update maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -36,15 +36,23 @@ func assertError(t *testing.T, got, want error) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
-	want := "this is just a test"
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
+	//dictionary.Add("test", "this is just a test")
+	//want := "this is just a test"
 
-	got, err := dictionary.Search("test")
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	if want != got {
+	if definition != got {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
 }
