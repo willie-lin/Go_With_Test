8d632f1b9d538ce1e88751b67acd52d687c370d2
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-01 19:13:30 +0800

    update maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -13,12 +13,9 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-		assertStrings(t, err.Error(), want)
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
 	})
 
 }
@@ -28,5 +25,26 @@ func assertStrings(t *testing.T, got, want string) {
 	if got != want {
 		t.Errorf("got '%s' want '%s' given, '%s'", got, want, "test")
 	}
+}
 
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error '%s' want '%s'", got, want)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+	want := "this is just a test"
+
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if want != got {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
 }
