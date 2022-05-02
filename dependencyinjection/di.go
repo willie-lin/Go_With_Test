53d6327d965552907ceec4a667941b0522da342e
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-02 21:22:44 +0800

    update di

--- dependencyinjection/di.go
+++ dependencyinjection/di.go
@@ -3,10 +3,16 @@ package dependencyinjection
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+
+	Greet(w, "World!")
 
 }
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/willie-lin/Go_With_Test/dependencyinjection"
+	"net/http"
 	"os"
 )
 
 func main() {
 	dependencyinjection.Greet(os.Stdout, "Elodie")
+
+	http.ListenAndServe(":5000", http.HandlerFunc(dependencyinjection.MyGreeterHandler))
 
 }
