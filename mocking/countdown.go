fb4555cfcf53cb93afc1696739447f0133d53689
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-03 21:24:34 +0800

    update mock

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -5,11 +5,14 @@ import (
 	"io"
 )
 
+const findWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
 
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
 
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, findWord)
 }
