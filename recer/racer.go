28f1f277ba775c0823dafd42278318a235579eb8
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-13 23:22:07 +0800

    update racer

--- recer/racer.go
+++ recer/racer.go
@@ -2,21 +2,22 @@ package recer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuraction := time.Since(startB)
-
-	if aDuration < bDuraction {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
+}
+
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
