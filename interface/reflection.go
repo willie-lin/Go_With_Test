a83c3248ea94d3482d95b1116054fb190b902d8f
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-31 22:12:52 +0800

    update racer

--- interface/reflection.go
+++ interface/reflection.go
@@ -1,1 +1,5 @@
 package _interface
+
+func walk(x interface{}, fn func(input string)) {
+	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+}
--- interface/reflection_test.go
+++ interface/reflection_test.go
@@ -19,7 +19,3 @@ func TestWalk(t *testing.T) {
 		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
 	}
 }
-
-func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
-}
