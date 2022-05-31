4139d417800bcc22ed2af2ffd910651581df4a26
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-31 22:23:11 +0800

    update racer

--- app/server.go
+++ app/server.go
@@ -1,9 +1,20 @@
 package app
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
 
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
-
-type HandlerFunc func(ResponseWriter, *Request)
