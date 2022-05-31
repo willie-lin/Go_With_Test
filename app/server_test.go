e3e192a54f2728f9e9421907bbe8404dd67d63c4
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-05-31 22:23:45 +0800

    update racer

--- app/server_test.go
+++ app/server_test.go
@@ -1,9 +1,40 @@
 package app
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
 
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "20")
+	})
+
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
 }
 
-func ListenAndServe(addr string, handler Handler) error
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got '%s' want '%s'", got, want)
+	}
+}
