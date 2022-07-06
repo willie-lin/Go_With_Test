7762a65b98eeb6ddf5801dcbf0516572c5ea6389
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2022-07-06 17:37:01 +0800

    strings
    Co-authored-by: willie-lin <yyhmmwan@gmail.com>

---
+++ Strings/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(
+		strings.Contains("test", "es"),
+
+		// does "test" begin with "te"?
+		strings.HasPrefix("test", "te"),
+
+		// does "test" end in "st"?
+		strings.HasSuffix("test", "st"),
+
+		// how many times is "t" in test?
+		strings.Count("test", "t"),
+
+		// at what position is "e" in "test"?
+		strings.Index("test", "e"),
+		// join "input" and "output" with "/"
+		strings.Join([]string{"input", "output"}, "/"),
+
+		// repeat "Golly" 6 times
+		strings.Repeat("Golly", 6),
+		/* replace "xxxx" with the first two non-overlapping instances of "a" replaced by "b" */
+		strings.Replace("xxxx", "a", "b", 2),
+		/* put "a-b-c-d-e" into a slice using "-" as a delimiter */
+		strings.Split("a-b-c-d-e", "-"),
+		// put "TEST" in lower case
+		strings.ToLower("TEST"),
+		// put "TEST" in upper case
+		strings.ToUpper("test"),
+	)
+
+}
